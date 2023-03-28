e1d6571ff4123d80b8cb7b745be4ea6491c3b98e
Egor Mikhalev <rasengan21196@gmail.com>
2023-03-28 18:56:51 +0300

    first commit

---
+++ src/test/java/com/postgres/integration/springpostgres/SpringPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postgres.integration.springpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
